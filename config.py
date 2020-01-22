18a82dd40c2d89fde6272146025d956c59f2a0bf
shenberg <shenberg@gmail.com>
2020-01-22 14:12:02 -0800

    missing files

---
+++ config.py
@@ -0,0 +1,6 @@
+
+# TODO: feature extraction needs some sample-rate aware love
+#       as librosa onset_strength() `feature` param expects 22050
+sample_rate = 22050
+# approx sample_rate/100
+hop_length = 220
